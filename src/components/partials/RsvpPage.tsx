db4fe8154e69147a155d67e0da040b7cf345a405
Tom Bird <thomas.bird1984@gmail.com>
2019-07-22 14:17:11 -0400

    Tweaking the rsvp page and adding better error handling

--- src/components/partials/RsvpPage.tsx
+++ src/components/partials/RsvpPage.tsx
@@ -11,12 +11,16 @@ interface Props extends RouteComponentProps {
 }
 
 interface State {
+    name: string | undefined;
     identifier: string | undefined;
     identifierDisabled: boolean;
     coming: CustomSelectItem;
     comingList: CustomSelectItem[];
     plusOne: CustomSelectItem;
     plusOneList: CustomSelectItem[];
+    plusOneName?: string;
+    isSubmitted: boolean;
+    isErrors: boolean;
 }
 
 export class RsvpPage extends React.Component<Props, State> {
@@ -24,6 +28,7 @@ export class RsvpPage extends React.Component<Props, State> {
         super(props, context);
 
         this.state = {
+            name: "",
             // @ts-ignore
             identifier: this.props.match && this.props.match.params && this.props.match.params.identifier && atob(this.props.match.params.identifier) || "",
             // @ts-ignore
@@ -55,7 +60,10 @@ export class RsvpPage extends React.Component<Props, State> {
                     val: "no",
                     text: "No"
                 }
-            ]
+            ],
+            plusOneName: "",
+            isSubmitted: false,
+            isErrors: false,
         };
     }
 
@@ -71,67 +79,125 @@ export class RsvpPage extends React.Component<Props, State> {
     public render(): JSX.Element {
         return (
             <Containers>
-                <h2>RSVP</h2>
-                <p>{this.state.identifier === undefined ? "undefined" : this.state.identifier}</p>
-                <p>We're so excited to take the next step in our journey together, as our special guests who mean so much to us we're extremely excited to see and celebrate together. Do us a favor and use the form below to let us know whether you'll be attending and if you'll be having a plus one, Thanks!</p>
-                <p>Afterwards go checkout our <Link to={"/p/music"}>music page</Link>, where you can request songs to be played during the reception!</p>
-
-                <form onSubmit={(e) => this.submit(e)}>
-                    <div className={"FormGroup"}>
-                        <label className={"FormGroup__label"} htmlFor={"identifier"}>Phone</label>
-                        <input
-                            className={"FormGroup__input"}
-                            type={"text"}
-                            name={"identifier"}
-                            id={"identifier"}
-                            disabled={this.state.identifierDisabled}
-                            value={this.state.identifier}
-                            onChange={(e) => this.setState({ identifier: e.target.value })}
-                        />
+                {this.state.isErrors ? (
+                    <div className={"rsvp__errors"}>
+                        Uh oh, something went wrong, you may not have entered the required information or something might have happened on the backend please try again!
                     </div>
+                ): (undefined)}
 
-                    <div className={"FormGroup"}>
-                        <label className={"FormGroup__label"} htmlFor={"makeit"}>Are you going to be able to make it?</label>
-                        <CustomSelect
-                            onChange={(item) => this.setState({ coming: item })}
-                            selected={this.state.coming}
-                            list={this.state.comingList}
-                        />
+                {this.state.isSubmitted ? (
+                    <div className={"rsvp__success"}>
+                        {this.state.coming.val === "yes" ? (
+                            <>
+                                <h2>Awesome we're so excited to hear your coming!</h2>
+                                <p>Thanks for taking the time to let us know you can make it, if you have time stop bye the <Link to={"/p/music"}>music page</Link> and request some songs! If you're needing accommodations for the night of the event there is a hotel right around the corner that has group pricing set up for us, just checkout the <Link to={"/p/accommodations"}>Accommodations page</Link> for more information.</p>
+                            </>
+                        ) : (
+                            <>
+                                <h2>We're sorry to hear your not able to make it.</h2>
+                                <p>We hoped you could make it, but we understand life can be busy. If things change you can always come back here and update us!</p>
+                            </>
+                        )}
                     </div>
+                ): (
+                    <>
+                        <h2>RSVP</h2>
 
-                    {this.state.coming.val === "yes" ? (
-                        <div className={"FormGroup"}>
-                            <label className={"FormGroup__label"} htmlFor={"plusOne"}>Do you have a plus one?</label>
-                            <CustomSelect
-                                onChange={(item) => this.setState({ plusOne: item })}
-                                selected={this.state.plusOne}
-                                list={this.state.plusOneList}
-                            />
-                        </div>
-                    ) : (undefined)}
+                        <p>We're so excited to take the next step in our journey together, as our special guests who mean so much to us we're extremely excited to see and celebrate together. Do us a favor and use the form below to let us know whether you'll be attending and if you'll be having a plus one, Thanks!</p>
+                        <p>Afterwards go checkout our <Link to={"/p/music"}>music page</Link>, where you can request songs to be played during the reception!</p>
 
-                    <button type={"submit"} className={"btn btn--primary"}>RSVP</button>
-                </form>
+                        <form onSubmit={(e) => this.submit(e)}>
+                            <div className={"container__row"}>
+                                <div className={"FormGroup"}>
+                                    <label className={"FormGroup__label"} htmlFor={"name"}>Name</label>
+                                    <input
+                                        className={"FormGroup__input"}
+                                        type={"text"}
+                                        name={"name"}
+                                        id={"name"}
+                                        value={this.state.name}
+                                        placeholder={"Enter your name... (optional)"}
+                                        onChange={(e) => this.setState({ name: e.target.value })}
+                                    />
+                                </div>
+
+                                <div className={"FormGroup"}>
+                                    <label className={"FormGroup__label"} htmlFor={"identifier"}>Phone</label>
+                                    <input
+                                        className={"FormGroup__input"}
+                                        type={"text"}
+                                        name={"identifier"}
+                                        id={"identifier"}
+                                        disabled={this.state.identifierDisabled}
+                                        value={this.state.identifier}
+                                        placeholder={"Enter your phone number..."}
+                                        onChange={(e) => this.setState({ identifier: e.target.value })}
+                                    />
+                                </div>
+                            </div>
+
+                            <div className={"FormGroup"}>
+                                <label className={"FormGroup__label"} htmlFor={"makeit"}>Are you going to be able to make it?</label>
+                                <CustomSelect
+                                    onChange={(item) => this.setState({ coming: item })}
+                                    selected={this.state.coming}
+                                    list={this.state.comingList}
+                                />
+                            </div>
+
+                            {this.state.coming.val === "yes" ? (
+                                <div className={"FormGroup"}>
+                                    <label className={"FormGroup__label"} htmlFor={"plusOne"}>Do you have a plus one?</label>
+                                    <CustomSelect
+                                        onChange={(item) => this.setState({ plusOne: item })}
+                                        selected={this.state.plusOne}
+                                        list={this.state.plusOneList}
+                                    />
+                                </div>
+                            ) : (undefined)}
+
+                            {this.state.plusOne.val === "yes" ? (
+                                <div className={"FormGroup"}>
+                                    <label className={"FormGroup__label"} htmlFor={"plusOneName"}>Plus one's name</label>
+                                    <input
+                                        className={"FormGroup__input"}
+                                        type={"text"}
+                                        name={"plusOneName"}
+                                        id={"plusOneName"}
+                                        value={this.state.plusOneName}
+                                        placeholder={"Enter plus one's name..."}
+                                        onChange={(e) => this.setState({ plusOneName: e.target.value })}
+                                    />
+                                </div>
+                            ) : (undefined)}
+
+                            <button type={"submit"} className={"btn btn--primary"}>RSVP</button>
+                        </form>
+                    </>
+                )}
             </Containers>
         );
     }
 
     private submit(e: any): void {
+        this.setState({ isErrors: false });
         if(isTruthy(this.state.identifier) && isTruthy(this.state.coming)) {
             const dataForRequest = {
+                name: this.state.name,
                 phone: this.state.identifier,
                 coming: this.state.coming.val,
                 plusOne: this.state.plusOne.val,
+                plusOneName: this.state.plusOneName,
             };
 
             axiosInstance
                 .post(`/rsvp-respond`, dataForRequest).then(response => {
                     console.log("response", response.data);
+                    this.setState({ isSubmitted: true });
                 });
         } else {
-            console.log("failed");
+            this.setState({ isErrors: true });
         }
-
         e.preventDefault();
     }
 }
